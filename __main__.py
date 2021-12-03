1ee8b93c50fdeff8e0e1757918fab34dccea45a8
Ilya udalov <ilya_udalov_04@mail.ru>
2021-12-03 23:44:53 +0300

    version 1.0

---
+++ __main__.py
@@ -0,0 +1,7 @@
+from triangle_constructor.triangle import Triangle
+
+if __name__ == '__main__':
+    triangle = Triangle()
+    triangle.enter_data()
+    triangle.get_count_of_triples()
+    triangle.get_variables_of_triangle()
